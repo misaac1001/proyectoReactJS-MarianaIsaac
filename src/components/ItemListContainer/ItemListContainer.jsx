201258a1babd0d8ce47e69a9a4f388db2d01c1a9
Mariana Isaac <marian.isaac10@gmail.com>
2023-12-14 13:49:10 -0300

    Firefase 1 casi completo

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";*/
 import { ItemList } from "./ItemList/ItemList";
 import { Loading } from "../Loading/Loading";
-import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, getFirestore } from "firebase/firestore";
 export const ItemListCountainer = ({ greeting }) => {
-  const [product, setProducts] = useState({});
+  const [products, setProducts] = useState({});
   const [loading, setLoading] = useState(true);
   /*   const [products, setProduct] = useState([]);
   
@@ -26,16 +26,33 @@ export const ItemListCountainer = ({ greeting }) => {
         .finally(() => setLoading(false));
     }
   }, [cid]); */
-  useEffect(() => {
+/*   useEffect(() => {
     const dbFirestore = getFirestore();
-    const queryDoc = doc(dbFirestore, "product", "8ybPJJhvuuF7FfqqdgBn");
+    const queryDoc = doc(dbFirestore, 'products', '8ybPJJhvuuF7FfqqdgBn' );
     getDoc(queryDoc)
-      .then((resultado) =>
-        setProducts({ id: resultado.id, ...resultado.data() })
-      )
-      .catch((err) => console.log(err));
+      .then((resultado) => {
+        if (resultado.exists()) {
+          setProducts({id: resultado.id, ...resultado.data()})
+          console.log(resultado)
+        } else {
+          console.log("No se encontraron datos para el ID proporcionado");
+        }
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
   }, []);
+  console.log(products)
+ */
+useEffect(()=> {
+  const dbFirestore = getFirestore()
+  const queryCollection = collection(dbFirestore, 'products' )
+  getDocs(queryCollection)
+  .then(resp=>setProducts(resp.docs.map(producto => ({id: producto.id, ...producto.data()}))))
+  .catch(err=>console.log(err))
+  .finally(()=>setLoading(false))
+}, []);
 
+console.log(products)
   return (
     <>
       <h2>Bienvenidos a: {greeting}</h2>
